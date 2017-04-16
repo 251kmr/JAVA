d880158099b805ba93f26124dfb694b72199cd40
kmr <251kmr@gmail.com>
2017-04-16 13:29:29 -0700

    SUM program

---
+++ Sum.java
@@ -0,0 +1,18 @@
+package basics;
+
+public class Sum {
+
+	public static void main(String[] args) {
+
+		int initialNo =1;
+		int sum=0;
+		while(initialNo<=10){
+			sum+=initialNo;
+			initialNo++;
+		}
+		System.out.println(sum);
+		
+	}
+
+}
+
